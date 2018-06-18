f9ff7d0edb22c41784c03ebfd05c940d40b6496d
FilaCo <dvchizha@edu.hse.ru>
2018-06-18 23:34:54 +0500

    Добавлена генерация графа и его вывод

---
+++ Задача_8_(37)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Задача_8__37_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph taskGraph = new Graph();
+            taskGraph.Show();
+        }
+    }
+}
