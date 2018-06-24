b0c8e33bf6ebe5bbe07e1e06f47189f838826cc8
FilaCo <dvchizha@edu.hse.ru>
2018-06-24 23:32:49 +0500

    Добавлены комментарии к исходному коду. Исправлена ошибка: был доступен ввод неположительного числа, что приводило к аварийному завершению программы

--- Задача_4_(119е)/Program.cs
+++ Задача_4_(119е)/Program.cs
@@ -8,31 +8,46 @@ namespace Задача_4__119е_
 {
     class Program
     {
-        static double InputNumber()
+        /// <summary>
+        /// Ввод действительного числа
+        /// </summary>
+        /// <returns></returns>
+        static double InputPosNumber()
         {
             double inpNum;
             bool check = false;
             do
             {
                 check = Double.TryParse(Console.ReadLine(), out inpNum);
-                if (!check)
-                    Console.WriteLine("Некорректный ввод. Необходимо ввести действительное число");
+                if (!check || inpNum <= 0)
+                {
+                    Console.WriteLine("Некорректный ввод. Необходимо ввести действительное число, большее 0");
+                    check = false;
+                }
             } while (!check);
             return inpNum;
         }
-        static double CountSum(ref double accuracy, int iter)
+        /// <summary>
+        /// Подсчёт суммы с заданной точностью
+        /// </summary>
+        /// <param name="accuracy">Точность вычислений</param>
+        /// <param name="iter">Номер члена ряда</param>
+        /// <returns></returns>
+        static double CountSum(double accuracy, int iter)
         {
+            // Вычисляем значение члена ряда с номером iter
             double curElem = 1 / (Math.Pow(4, iter) + Math.Pow(5, iter + 2));
+            // Сравниваем его абсолютное значение с точностью
             if (Math.Abs(curElem) < accuracy)
                 return curElem;
             else
-                return curElem + CountSum(ref accuracy, iter + 1);
+                return curElem + CountSum(accuracy, iter + 1);
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Задайте точность вычислений:");
+            Console.WriteLine("Задайте точность вычислений (eps > 0):\nP.S. N-ый член суммы будет меньше, чем eps");
             double eps = InputNumber();
-            double sum = CountSum(ref eps, 1);
+            double sum = CountSum(eps, 0);
             Console.WriteLine("Точность: {0}\nСумма:{1}", eps,sum);
         }
     }
