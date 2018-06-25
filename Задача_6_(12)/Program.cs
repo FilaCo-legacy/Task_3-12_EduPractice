17d8b8c318c49bcc0f9c5c0789a631d11d348ca9
FilaCo <dvchizha@edu.hse.ru>
2018-06-25 05:16:59 +0500

    Исправил ошибку: необходима была рекурсивная реализация, а сделана итеративная

--- Задача_6_(12)/Program.cs
+++ Задача_6_(12)/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Задача_6__12_
 {
     class Program
     {
+        /// <summary>
+        /// Массив, хранящий первые 2N элементов последовательности
+        /// </summary>
+        static double[] arr;
+        /// <summary>
+        /// Ввод действительного числа
+        /// </summary>
+        /// <returns></returns>
         static double InputNumber()
         {
             double inpNum;
@@ -20,6 +24,10 @@ namespace Задача_6__12_
             } while (!check);
             return inpNum;
         }
+        /// <summary>
+        /// Ввод натурального числа
+        /// </summary>
+        /// <returns></returns>
         static int InputNaturalNumber()
         {
             int inpNum;
@@ -35,6 +43,12 @@ namespace Задача_6__12_
             } while (!check);
             return inpNum;
         }
+        /// <summary>
+        /// Сравнение двух действительных чисел
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="a2"></param>
+        /// <returns></returns>
         static int Compare(double a1, double a2)
         {
             if (a1 < a2)
@@ -43,46 +57,53 @@ namespace Задача_6__12_
                 return -1;
             return 0;
         }
-        static bool CheckSequence(double a1, double a2, double a3, int N, out double [] arr)
+        /// <summary>
+        /// Заполнение массива arr элементами
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        static double Elem(int num)
         {
-            arr = new double[2 * N-1];
-            arr[0] = a1;
-            if (N == 1)
-                return true;
-            arr[1] = a2;
-            arr[2] = a3;
-            if (N == 2)
-                return true;
-            int cmp = Compare(a1, a3);
-            bool flag = true;
-            for (int i = 3; i < 2*N-1; i++)
+            if (num < 3)
+                return arr[num];
+            arr[num] = 0.7 * Elem(num - 1) - 0.2 * Elem(num - 2) + num * Elem(num - 3);
+            return arr[num];
+        }
+        /// <summary>
+        /// Проверка последовательности первых N элементов стоящих на нечётных местах на монотонность
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        static bool CheckSequence(int N)
+        {
+            int cmp = Compare(arr[0], arr[2]);
+            for (int i = 3; i < 2 * N-1; i+=2)
             {
-                arr[i] = 0.7 * arr[i - 1] - 0.2 * arr[i - 2] + (i + 1) * arr[i - 3];
-                if (i % 2 == 0 && Math.Abs(Compare(arr[i - 2], arr[i]) - cmp) == 2)
-                    flag = false;
+                if (cmp == 0 && Compare(arr[i - 2], arr[i]) != 0)
+                    cmp = Compare(arr[i - 2], arr[i]);
+                else if (Math.Abs(cmp - Compare(arr[i - 2], arr[i])) > 1)
+                    return false;
             }
-            return flag;
+            return true;
         }
         static void Main(string[] args)
         {
-            double a1, a2, a3;
-            int N;
-            double[] arr;
-            Console.WriteLine("Введите первые три элемента последовательности:");
-            a1 = InputNumber();
-            a2 = InputNumber();
-            a3 = InputNumber();
             Console.WriteLine("Введите число N:");
-            N = InputNaturalNumber();
-            if (CheckSequence(a1, a2, a3, N, out arr))
+            int N = InputNaturalNumber();
+            arr = new double[Math.Max(3,2 * N-1)];
+            Console.WriteLine("Введите первые три элемента последовательности:");
+            for (int i = 0; i < 3; ++i)
+                arr[i] = InputNumber();
+            Elem(2 * N-2);
+            if (CheckSequence(N))
                 Console.WriteLine("Подпоследовательность первых N элементов на " +
                 "нечётных местах монотонна");
             else
                 Console.WriteLine("Подпоследовательность первых N элементов на " +
                 "нечётных местах не монотонна");
             Console.WriteLine("\nСама последовательность:");
-            foreach (double x in arr)
-                Console.Write("{0} ", x);
+            foreach (var x in arr)
+                Console.Write("{0:F4} ", x);
         }
     }
 }
